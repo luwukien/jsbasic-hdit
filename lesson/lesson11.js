55da97911d987270ffd5c56ced3de4e468f5adc9
KienChi Luu <luukien2910@gmail.com>
2023-07-22 01:40:52 +0700

    rang sang 22/7

---
+++ lesson/lesson11.js
@@ -0,0 +1,21 @@
+//#11 backstick & string templates
+
+
+// let firstName = "Quynh";
+// let lastName = "Truc";
+
+// let text = `Welcome ${firstName}, ${lastName}!`;
+
+// console.log(text)
+
+
+let a = 5;
+
+let b = 10;
+
+console.log(`fifteen is ` + (a + b ) + ' and\nnot' + ( 2 * a  + b ) + ' .');
+console.log(typeof(a +b ));
+
+console.log(` print variable a: a = ${a}, b =  ${b}, c(a +b) = ${a + b}`)
+
+
---
+++ lesson/lesson12.js
@@ -0,0 +1,22 @@
+// # 12 object
+
+// 1 object include key, value
+// key: value 
+// note mỗi 1 object dược ngăn cách nhau bằng 1 dấu chấm phẩy (;)
+// key không được dùng dấu cách ,ex) full name: "luuchikien" 
+
+
+let obj = {
+    name: " luu kien",
+
+    date: ' 29/10/ 2006'
+
+};  
+
+let b = ' luu kien';
+
+obj.name = "dat "
+
+console.log('what is your name ', ',my name is ', obj.name)
+
+console.log("what is your birthday ", ",my birthday is  ", obj.date)
